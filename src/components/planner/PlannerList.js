80b2a06e0e07ae1651f07f09da0125d184568093
Hoan Phung <hoanphung.ptso1@gmail.com>
2018-12-13 16:26:06 +0100

    planner styling

--- src/components/planner/PlannerList.js
+++ src/components/planner/PlannerList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { bold } from 'ansi-colors';
 
 export const PlannerList = (props) => {
     console.log(props.planner)
@@ -10,8 +11,8 @@ export const PlannerList = (props) => {
         <div>
             <h1>YOUR WEEKLY PLANNER</h1>
             <h4>DINNER</h4>
-            <button>Add all to Shopping List</button>
-            <div className='row'>
+            <button className='button'>Add all to Shopping List</button>
+            {/* <div className='row'>
                 {props.planner.map(menu => {
                     return (
                         <div className='col s2'>
@@ -19,11 +20,25 @@ export const PlannerList = (props) => {
                         </div>
                     )
                 })}
-                
-        
-
-            </div>
-            <a className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons">add</i></a>
+            </div> */}
+            <ul>
+                {props.planner.map(menu => {
+                    return (
+                        <li className='planner' key={menu.id}>
+                            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZQoE0L43jH6eh5DxXUaT7TPzIzjb2OcxiXyAcMFqIprusSCkCKw' width='200px' height='160px'/>
+                            <h3 style={{fontWeight: 'bold'}}>NAME OF THE MEAL</h3>
+                            <span>COOKING TIME</span><br></br>
+                            <i class="small material-icons">star_border star_border star_border star_border star_border</i><br></br>
+                            <span>NUTRITION INFO</span><br></br>
+                            <button className='change-button'>CHANGE</button>
+                            <button className='add-to-shopping-list-button'>ADD TO LIST</button>
+                        </li>
+                    )
+                })}
+                <li className='add-button'>
+                    <a className="btn-floating btn-large grey"><i className="material-icons">add</i></a>
+                </li>
+            </ul>
         </div>
     )
 }
